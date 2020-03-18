a329251e6f7b13901e779652e53ddae7f02c0b93
BayernMuller <cjjunk29@gmail.com>
2020-03-18 23:29:45 +0900

    Update WindowFinder - Use template class

--- WindowFinder/WindowsFinder.h
+++ WindowFinder/WindowsFinder.h
@@ -3,18 +3,30 @@
 #include <vector>
 using namespace std;
 
+template<class Container = vector<HWND>>
 class WindowsFinder
 { 
 public:
-	static void Find(vector<HWND>* set)
+	static bool Find(Container* set)
 	{
-		EnumWindows(&WindowsFinder::GetWindowsList, LPARAM(set));
+		if (IsWrongType())
+			return false;
+		return EnumWindows(&WindowsFinder::GetWindowsList, LPARAM(set));
 	}
 	
+	static bool Find(WNDENUMPROC lpEnumFunc, Container* set)
+	{
+		if (IsWrongType())
+			return false;
+		return EnumWindows(lpEnumFunc, LPARAM(set));
+	}
+
+	
+
 private:
 	static BOOL __stdcall GetWindowsList(HWND hWnd, LPARAM lParam)
 	{
-		auto* vec = reinterpret_cast<vector<HWND>*>(lParam);
+		auto* con = reinterpret_cast<Container*>(lParam);
 		if (::IsWindow(hWnd))
 		{
 			if (!::IsWindowVisible(hWnd))
@@ -23,10 +35,15 @@ private:
 				return TRUE;
 			if (::GetParent(hWnd) != 0)
 				return TRUE;
-			vec->push_back(hWnd);
+			con->push_back(hWnd);
 			return TRUE;
 		}
 		return FALSE;
+	}
+
+	static bool IsWrongType()
+	{
+		return typeid(Container::value_type) != typeid(HWND);
 	}
 };
 
