e8234c09e737fd900bd6f8c132936a6d13fc48b9
BayernMuller <cjjunk29@gmail.com>
2020-03-28 20:29:43 +0900

    Searching Kospi Kosdaq API

--- KospiKosdaq/widget.py
+++ KospiKosdaq/widget.py
@@ -1,4 +1,11 @@
+import sys
 import requests
-url = 'https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=%EC%BD%94%EC%8A%A4%ED%94%BC+%EC%BD%94%EC%8A%A4%EB%8B%A5'
-res = requests.get(url)
-print(res.text)
+import json
+url = 'http://ecos.bok.or.kr/api/StatisticItemList/sample/json/kr/1/9/064Y001/0089000/'
+response = requests.get(url)
+if not response:
+	print('Failed to request')
+	sys.exit(0)
+
+string = json.dumps(json.loads(response.text), indent=4, ensure_ascii=False)
+print(string)
